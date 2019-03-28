28b7abea72adbbdbe3bfe44eb36ffaad468b745a
JasonMacKeigan <jay_6955@hotmail.com>
2019-03-28 19:41:25 -0300

    Update AcmeController value

--- src/main/java/jenkinsgit/controller/AcmeController.java
+++ src/main/java/jenkinsgit/controller/AcmeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AcmeController {
 
-    @GetMapping("/.well-known/acme-challenge/v86nYz6cPZyPX-4Nde_ERQN0M1dmTjpdkxphmPJx5oU")
+    @GetMapping("/.well-known/acme-challenge/AA1fpCwJqQcXavYtr70Q0PidHPMGkYCTvzNhovur56A")
     public String get() {
         return "someValue";
     }
