fbb4a7f5ebb9c20faec280d864b3bdeb093e7e6e
JasonMacKeigan <jay_6955@hotmail.com>
2019-03-28 17:57:38 -0300

    Update AcmeController

--- src/main/java/jenkinsgit/controller/AcmeController.java
+++ src/main/java/jenkinsgit/controller/AcmeController.java
@@ -2,14 +2,15 @@ package jenkinsgit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Jason MK on 2019-03-28 at 5:35 PM
  */
-@Controller
+@RestController
 public class AcmeController {
 
-    @GetMapping("/.well-known/acme-challenge/")
+    @GetMapping("/.well-known/acme-challenge/v86nYz6cPZyPX-4Nde_ERQN0M1dmTjpdkxphmPJx5oU")
     public String get() {
         return "someValue";
     }
