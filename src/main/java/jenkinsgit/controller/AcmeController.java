977f23648d7a9e8afc5103dbb2b98063c5b8c4e2
JasonMacKeigan <jay_6955@hotmail.com>
2019-03-28 20:43:54 -0300

    Update ACME controller so that it allows wildcard get requests

--- src/main/java/jenkinsgit/controller/AcmeController.java
+++ src/main/java/jenkinsgit/controller/AcmeController.java
@@ -1,8 +1,13 @@
 package jenkinsgit.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by Jason MK on 2019-03-28 at 5:35 PM
@@ -10,9 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AcmeController {
 
-    @GetMapping("/.well-known/acme-challenge/AA1fpCwJqQcXavYtr70Q0PidHPMGkYCTvzNhovur56A")
-    public String get() {
-        return "someValue";
+    @GetMapping(value = "/.well-known/acme-challenge/**")
+    public String get(HttpServletRequest request) {
+        String url = request.getRequestURL().toString();
+
+        int indexOfSplitter = url.lastIndexOf("/");
+
+        if (indexOfSplitter != -1) {
+            url = url.substring(indexOfSplitter + 1);
+        }
+        Path file = Paths.get("templates", ".well-known", "acme-challenge", url);
+
+        System.out.println(String.format("File exists: %s", Files.exists(file)));
+
+        if (Files.exists(file)) {
+            try {
+                System.out.println("File content: " + Files.readAllLines(file));
+            } catch (IOException e) {
+                System.out.println("IOException occured while reading all lines.");
+                e.printStackTrace();
+                // bad practice to fail silently but i need to for this :(
+            }
+        }
+        return url;
     }
 
 }
